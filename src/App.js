29531bbbd9df65519e26b893d87464d76520b96c
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-10-26 17:37:04 +0200

    install tailwindCSS and delete some react default images and files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import "./App.css";
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">learn</header>
+    <div className="text-center">
+      <header className="bg-[#282c34] min-h-screen flex flex-col items-center justify-center text-white text-[calc(10px+2vmin)]">
+        <img
+          src="logo.svg"
+          className="h-[40vmin] pointer-events-none animate-spin-slow"
+          alt="logo"
+        />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        
+      </header>
     </div>
   );
 }
